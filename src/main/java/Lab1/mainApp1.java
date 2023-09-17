a2ebcf8744436130aa8db3c1f6ab4727ed8701df
wesleyman <lcman@ust.hk>
2023-09-17 16:38:44 +0800

    Add 3rd print line

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -7,5 +7,6 @@ public class mainApp1 {
         System.out.println("Program started ...");
         System.out.println(b+" to power "+n+" = "+ myLibrary.Power(b,n));
         System.out.println(n+"! = "+myLibrary.factorial(n));
+        System.out.println("Thank you for using Scientific Calculator!");
     }
 }
